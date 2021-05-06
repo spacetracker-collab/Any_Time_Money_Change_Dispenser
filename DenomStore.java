b272148ee33a62ec859fce36aa475fcbed1e7fc8
spacetracker-collab <83755715+spacetracker-collab@users.noreply.github.com>
2021-05-06 14:12:58 +0530

    Create DenomStore.java

---
+++ DenomStore.java
@@ -0,0 +1,35 @@
+package test;
+
+public class DenomStore {
+	
+	
+	  public static int getDenom20() {
+		return denom20;
+	}
+	public static void setDenom20(int denom20) {
+		DenomStore.denom20 = denom20;
+	}
+	public static int getDenom10() {
+		return denom10;
+	}
+	public static void setDenom10(int denom10) {
+		DenomStore.denom10 = denom10;
+	}
+	public static int getDenom5() {
+		return denom5;
+	}
+	public static void setDenom5(int denom5) {
+		DenomStore.denom5 = denom5;
+	}
+	public static int getDenom1() {
+		return denom1;
+	}
+	public static void setDenom1(int denom1) {
+		DenomStore.denom1 = denom1;
+	}
+	private static int denom20=0;
+	  private static int denom10=0;
+	  private static int denom5=0;
+	  private static int denom1=0;
+
+}
